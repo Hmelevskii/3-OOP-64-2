ed6229e1d5fde6244f9367aa09acd20f1a808168
Hmelevskii <limon.u@mail.ru>
2026-04-21 04:26:38 +0600

    Добавлена кнопка со случайными именами

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import flet as ft
 from datetime import datetime
+import random
+
 def main_page(page: ft.Page):
     page.title = 'Мое первое приложение'
     page.theme_mode = ft.ThemeMode.LIGHT
@@ -7,13 +9,12 @@ def main_page(page: ft.Page):
     text_hello = ft.Text(value='Hello world')
 
     greeting_history = []
-
+    names = []
+    random_names = ["Алексей", "Мария", "Иван", "Ольга"]
     history_text = ft.Text(value='История приветствий: ')
-
-
+    names_text = ft.Text(value='Любимые имена: ')
     text_hello.value = 'Hello Geeks'
     # text_hello = 'Hello world'
-
 
     def on_button_click(_):
         print(name_input.value)
@@ -29,6 +30,9 @@ def main_page(page: ft.Page):
             name_input.value = None
 
             greeting_history.append(text)
+            greeting_history[:] = greeting_history[-5:]
+
+
             # print(greeting_history)
 
             history_text.value = "История приветствий\n" + '\n'.join(greeting_history)
@@ -36,24 +40,43 @@ def main_page(page: ft.Page):
             text_hello.color = ft.Colors.RED
             text_hello.value = 'Введите имя'
             print('Ничего не ввели')
+    
+    def add_favorit_names(_):
+        if greeting_history:
+            favorit_name = greeting_history[-1]
+            if "Привет, " in favorit_name: 
+                name = favorit_name[favorit_name.find('Привет, ') + len('Привет, '):]
+                if name not in names:
+                    names.append(name)
+                    names_text.value = 'Любимые имена:\n'+'\n'.join(names)
 
+
+    def clear_history(_):
+        greeting_history.clear()
+        history_text.value = 'История приветствий: '
+        
     def switch_theme(_):
         if page.theme_mode == ft.ThemeMode.LIGHT:
             page.theme_mode = ft.ThemeMode.DARK
         else:
             page.theme_mode = ft.ThemeMode.LIGHT
-
+    
+    def set_random_names(_):
+        name_input.value = random.choice(random_names)
+        
     name_input = ft.TextField(on_submit=on_button_click, label='Введите имя', expand=True)
     elavated_button = ft.ElevatedButton('send', icon=ft.Icons.SEND, color=ft.Colors.RED, 
                                         icon_color=ft.Colors.GREEN, on_click=on_button_click)
     
-
+    favorit_button = ft.ElevatedButton('Добавить в избранное', on_click=add_favorit_names)
+    clear_button = ft.ElevatedButton('Очистить историю', on_click=clear_history)
     switch_button = ft.IconButton(icon=ft.Icons.BRIGHTNESS_6, on_click=switch_theme) 
-
-    main_object = ft.Row([name_input, elavated_button, switch_button])
-
-    page.add(text_hello, main_object, history_text)
-
+    random_button = ft.ElevatedButton("Рандомное имя", on_click=set_random_names)
+    main_object = ft.Row([name_input, elavated_button, switch_button, random_button])
+    history_row = ft.Row([history_text, clear_button, favorit_button])
+    favorit_row = ft.Row([names_text])
+    
+    page.add(text_hello, main_object, history_text, history_row, favorit_row)
 
 ft.app(main_page)
 # ft.app(main_page, view=ft.AppView.WEB_BROWSER)
