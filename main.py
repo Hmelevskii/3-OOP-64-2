70ec6417ceec46c2e16c87bc191782019409cb81
Hmelevskii <limon.u@mail.ru>
2026-04-21 04:50:48 +0600

    Добавлена кнопка удаления задачи

--- main.py
+++ main.py
@@ -63,6 +63,11 @@ def main_page(page: ft.Page):
     
     def set_random_names(_):
         name_input.value = random.choice(random_names)
+
+    def delete_history(_):
+        if greeting_history:
+            greeting_history.pop()
+            history_text.value = 'История приветсвий\n' + '\n'.join(greeting_history)
         
     name_input = ft.TextField(on_submit=on_button_click, label='Введите имя', expand=True)
     elavated_button = ft.ElevatedButton('send', icon=ft.Icons.SEND, color=ft.Colors.RED, 
@@ -71,9 +76,10 @@ def main_page(page: ft.Page):
     favorit_button = ft.ElevatedButton('Добавить в избранное', on_click=add_favorit_names)
     clear_button = ft.ElevatedButton('Очистить историю', on_click=clear_history)
     switch_button = ft.IconButton(icon=ft.Icons.BRIGHTNESS_6, on_click=switch_theme) 
-    random_button = ft.ElevatedButton("Рандомное имя", on_click=set_random_names)
+    random_button = ft.ElevatedButton("Рандомное имя", color=ft.Colors.RED, on_click=set_random_names)
+    delete_button = ft.ElevatedButton("Удалить задачу", on_click=delete_history)
     main_object = ft.Row([name_input, elavated_button, switch_button, random_button])
-    history_row = ft.Row([history_text, clear_button, favorit_button])
+    history_row = ft.Row([history_text, clear_button, favorit_button, delete_button])
     favorit_row = ft.Row([names_text])
     
     page.add(text_hello, main_object, history_text, history_row, favorit_row)
